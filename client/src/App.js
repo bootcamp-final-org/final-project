5f282b1f36236415d91edc905d04522415efb251
Haylee McLemore <hmmclemore@gmail.com>
2020-07-08 21:36:11 -0500

    updating navigation links and defining elements on homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Home from "./components/Home/Home";
 import Login from "./components/Auth/Login";
 import SignUp from "./components/Auth/SignUp";
 
@@ -11,9 +11,9 @@ function App() {
   return (
     <Router>
       <div>
-        <Nav />
+        <Nav login={true? "Login" : "logout"} about="About" logo="TFH" logout="Logout"/>
         <Switch>
-          <Route exact path="/" component={Books} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/books/:id" component={Detail} />
