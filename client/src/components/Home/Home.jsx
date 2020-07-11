392bfeebe0444d2c90c75cba9a334f7ebea5fb3b
Haylee McLemore <hmmclemore@gmail.com>
2020-07-11 14:07:39 -0500

    updating homepage to redirect to signup page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,28 +1,29 @@
 import React, { Component } from "react";
 import "./style.css";
+import { Link } from "react-router-dom";
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-            <div className="container">
-         
+        <div className="container">
           <main className="hero-content">
-              <div className="row">
+            <div className="row">
               <div className="col-sm-9">
-          <h1 className="hero-h1">Teaching assistance for your child. Anywhere. Anytime.</h1>
-            <p className="hero-p">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque,
-              exercitationem vitae sequi error repudiandae quasi necessitatibus
-              illum. Hic, asperiores totam amet mollitia in repudiandae eligendi
-              ipsum non. Est, veritatis, saepe!
-            </p>
-            <button className="signup">Sign Up</button>
-            </div>
+                <h1 className="hero-h1">
+                  Teaching assistance for your child. Anywhere. Anytime.
+                </h1>
+                <p className="hero-p">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Neque, exercitationem vitae sequi error repudiandae quasi
+                  necessitatibus illum. Hic, asperiores totam amet mollitia in
+                  repudiandae eligendi ipsum non. Est, veritatis, saepe!
+                </p>
+                <Link to="/signup" id="signup">Sign Up</Link>
+              </div>
             </div>
           </main>
-          </div>
-
+        </div>
       </div>
     );
   }
