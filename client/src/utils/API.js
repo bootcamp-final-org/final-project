85e5847632bc21b4687f67b74bfbb481b20bbae8
Haylee McLemore <hmmclemore@gmail.com>
2020-07-13 21:45:00 -0500

    testing signup

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,18 +3,18 @@ import axios from "axios";
 export default {
   // Gets all users
   getUsers: function() {
-    return axios.get("/api/users");
+    return axios.get("/api/students");
   },
   // Gets the user with the given id
   getUser: function(id) {
-    return axios.get("/api/users/" + id);
+    return axios.get("/api/students/" + id);
   },
   // Deletes the user with the given id
   deleteBook: function(id) {
-    return axios.delete("/api/users/" + id);
+    return axios.delete("/api/students/" + id);
   },
   // Saves a user to the database
   saveUser: function(userData) {
-    return axios.post("/api/users", userData);
+    return axios.post("/api/students", userData);
   }
 };
