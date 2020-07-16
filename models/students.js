f5495282267591c8c0ac982115998e17568819e4
Haylee McLemore <hmmclemore@gmail.com>
2020-07-15 19:54:56 -0500

    modifying login component and routes

--- models/students.js
+++ models/students.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const studentsSchema = new Schema({
-  userName: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
   password: { type: String, required: true },
   synopsis: String,
   date: { type: Date, default: Date.now },
